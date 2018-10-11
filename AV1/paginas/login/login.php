4cdaec7a793d3450dbde585222cbe2da4a1f9996
Murilo Andrade <muriloandradealmeida@gmail.com>
2018-10-11 19:15:00 -0300

    Primeira tela de login

---
+++ AV1/paginas/login/login.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <title>Login do Sistema</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <link rel="stylesheet" href="/css/login.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <div class="container">
+            <h2>Escolha uma opção:</h2><br>
+            <div class="centraliza">
+                <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-user"></span> Login ADM</a>
+                <a href="#" class="btn btn-default"><span class="glyphicon glyphicon-user"></span> Login Operador</a>
+            </div>
+        </div>
+    </body>
+</html>
